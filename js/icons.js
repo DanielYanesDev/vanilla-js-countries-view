e26321faac7e150c22db75971fdb75267a0352ee
EduardYan <eduarygp@gmail.com>
2025-07-31 18:12:40 -0600

    ThemeToggle button added

---
+++ js/icons.js
@@ -0,0 +1,20 @@
+export const LIGHT_THEME_TOGGLE_ICON = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="#1C274C"/>
+  </svg>
+`;
+
+export const DARK_THEME_TOGGLE_ICON = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+       xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="5" fill="#fff"/>
+    <line x1="12" y1="1" x2="12" y2="3" stroke="#fff" stroke-width="2"/>
+    <line x1="12" y1="21" x2="12" y2="23" stroke="#fff" stroke-width="2"/>
+    <line x1="1" y1="12" x2="3" y2="12" stroke="#fff" stroke-width="2"/>
+    <line x1="21" y1="12" x2="23" y2="12" stroke="#fff" stroke-width="2"/>
+    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" stroke="#fff" stroke-width="2"/>
+    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" stroke="#fff" stroke-width="2"/>
+    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" stroke="#fff" stroke-width="2"/>
+    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" stroke="#fff" stroke-width="2"/>
+  </svg>
+`;
